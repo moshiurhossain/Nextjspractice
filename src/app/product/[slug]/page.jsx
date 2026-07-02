b3ecdf975dc66b18780321e1663a77a2cc87e069
unknown <pantho883@gmail.com>
2026-07-03 03:35:24 +0600

    dynamic route added

--- src/app/product/[slug]/page.jsx
+++ src/app/product/[slug]/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>Dynamic page</div>
+const page =  async ({params}) => {
+    console.log(params)
+    const {slug} = await params
+    console.log(slug)
+    
+    return (
+    <div>Dynamic page: {slug}</div>
   )
 }
 
