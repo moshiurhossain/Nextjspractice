20e792fc41a675f4ab6f6cd73a7648f02c1c1590
unknown <pantho883@gmail.com>
2026-07-03 02:53:23 +0600

    pages created

---
+++ src/app/product/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Dynamic page</div>
+  )
+}
+
+export default page
---
+++ src/app/shop/menshop/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>menshop</div>
+  )
+}
+
+export default page
