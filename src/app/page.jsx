39162a838089fa25ab9e50301d550de4c6f9778f
unknown <pantho883@gmail.com>
2026-07-05 02:50:39 +0600

    nav head and footer component created

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+        <Header />
+        <Navbar />
+        <Footer />
+
+    </main>
+  )
+}
+
+export default page
